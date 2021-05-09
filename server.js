dfc71b716a5fb95c7f23c8f43a9946e843581e9d
Shubham Kamat <kamat.shubham@gmail.com>
2021-05-09 08:27:47 -0400

    Minor changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // We create a folder called client in the ZodiacApp dir to store our front end files. Now our back-end is connected to our front-end.
 app.use(express.static(__dirname + '/client'));
 
-app.use(requireHTTPS);
+//app.use(requireHTTPS);
 
 // set up an environment variable for the port for deployment and 3000 for running on localhost
 const port = process.env.PORT || 3000;
